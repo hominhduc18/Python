557bfd7353e9bb8040906fa89e91064e2199cdf5
duchm <duchm@tecacom.intern>
2022-10-05 21:14:47 +0700

    first commit

--- FastAPI/backend/app/frontend.py
+++
@@ -1,2 +0,0 @@
-from fastapi.templating import Jinja2Templates
-templates = Jinja2Templates(directory="/home/teca/workspace/app_0.2/frontend/templates")
--- FastAPI/backend/app/routes/api_v1.py
+++
@@ -1,9 +0,0 @@
-import sys
-sys.path.append(".")
-from fastapi import APIRouter
-from app.routes.api import root, auth, admin
-api_router = APIRouter()
-api_router.include_router(root.router, tags=["root"])
-api_router.include_router(auth.router, tags=["auth"])
-api_router.include_router(admin.router, prefix="/admin", tags=["admin"])
-# api_router.add_api_websocket_route("/sys")
